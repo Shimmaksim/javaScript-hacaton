61f20cedf51c3de9d146ff082cf284f06685ee56
shimmaksim <shimmaksim@gmail.com>
2019-12-06 17:52:40 +0200

    qwerty

---
+++ HakatonJS-master/src/js/index.js
@@ -0,0 +1,12 @@
+import '../css/styles.css';
+import sectionReg from './sectionRegister'
+import sectionMap from './sectionMap'
+import sectionLogin from './sectionLogin'
+import sectionAddPhoto from './sectionAddPhoto.js'
+import swiper from './swiper.js'
+sectionReg()
+sectionMap()
+sectionLogin()
+sectionAddPhoto()
+swiper()
+
